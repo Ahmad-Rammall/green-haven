557d6966a83b7b93b27129ded517162fdebcade1
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-26 00:39:22 +0200

    Add Product Routes

---
+++ green-haven-backend/routes/product.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { getAllSellerProducts, addProduct, updateProduct, deleteProduct, getOneProduct} = require("../controllers/auth.controllers");
+const router = express.Router();
+
+router.get("/", getAllSellerProducts);
+router.post("/", addProduct);
+router.put("/", updateProduct);
+router.delete("/:id", deleteProduct);
+router.get("/:id", getOneProduct);
+
+module.exports = router;
