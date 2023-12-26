49e81039aa391638d776bee09e6de19487791341
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-26 15:22:13 +0200

    Add AddProductToCart API

--- green-haven-backend/controllers/cart.controllers.js
+++ green-haven-backend/controllers/cart.controllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const Product = require("../models/product.model");
 
 const getUserCart = async (req, res) => {
   if (req.user.role !== "user") {
@@ -8,13 +9,41 @@ const getUserCart = async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.user._id });
     return res.status(200).json({ cart: user.cart });
-
   } catch (error) {
     return res.status(500).json(error);
   }
 };
 
-const addProductToCart = async (req, res) => {};
+const addProductToCart = async (req, res) => {
+  if (req.user.role !== "user") {
+    return res.status(403).send("Unauthorized");
+  }
+
+  try {
+    const user = req.user;
+    const { productId, quantity } = req.body;
+
+    const product = await Product.findOne({ _id: productId });
+
+    // Test if product exists in the user's cart
+    const existingCartItem = user.cart.find((item) =>
+      item.product.equals(productId)
+    );
+    if (existingCartItem) {
+      return res
+        .status(400)
+        .json({ message: "Product already exists in the cart" });
+    }
+
+    // Push plant to the user's cart array
+    user.cart.push({ product, quantity });
+
+    await user.save();
+    res.status(200).json({ message: "Product Added To Cart", cart: user.cart });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 const removeProductFromCart = async (req, res) => {};
 
