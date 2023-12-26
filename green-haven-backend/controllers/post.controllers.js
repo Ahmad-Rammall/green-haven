ffc7ad9e8f6b35e88b116e3a3a3eef68522d97b6
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-26 18:54:36 +0200

    Add DeletePost + GetUserPost APIs

--- green-haven-backend/controllers/post.controllers.js
+++ green-haven-backend/controllers/post.controllers.js
@@ -16,7 +16,12 @@ const updatePost = async (req, res) => {
 
   try {
     const post = await Post.findOne({ _id: postId });
-    console.log("Req: " + req.user._id + " postUser: " + post.user._id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post Doesnt Exist" });
+    }
+
+    // Verify that the post belongs to the authenticated user
     if (String(post.user._id) !== String(req.user._id)) {
       return res.status(403).json({ message: "Can't Change Other's Posts" });
     }
@@ -31,11 +36,39 @@ const updatePost = async (req, res) => {
   }
 };
 
-const deletePost = async (req, res) => {};
+const deletePost = async (req, res) => {
+  const { postId } = req.body;
+
+  try {
+    const post = await Post.findOne({ _id: postId });
+
+    if (!post) {
+      return res.status(404).json({ message: "Post Doesnt Exist" });
+    }
+
+    // Verify that the post belongs to the authenticated user
+    if (String(post.user._id) !== String(req.user._id)) {
+      return res.status(403).json({ message: "Can't Delete Other's Posts" });
+    }
+
+    await Post.findOneAndDelete({ _id: post._id });
+    return res.status(200).json({ message: "Post Delted" });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
 
 const likePost = async (req, res) => {};
 
-const getUserPosts = async (req, res) => {};
+const getUserPosts = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const posts = await Post.find({ user: userId });
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
 
 const getFollowingPosts = async (req, res) => {};
 
