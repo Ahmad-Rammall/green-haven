6bd7223efff4f3b6e1250f968ff77cec88049f84
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-26 12:58:58 +0200

    Remove Seller Model + Update User Model

--- green-haven-backend/models/user.model.js
+++ green-haven-backend/models/user.model.js
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "admin"],
+      enum: ["user", "seller", "admin"],
       default: "user",
       required: true,
     },
