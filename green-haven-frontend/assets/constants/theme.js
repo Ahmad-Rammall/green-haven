461fae6f41ddb0ecb0fa6c3b6750f18d143d59a5
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-29 15:45:45 +0200

    Create Assets Folder

---
+++ green-haven-frontend/assets/constants/theme.js
@@ -0,0 +1,57 @@
+
+import { Dimensions } from 'react-native'
+const { height, width } = Dimensions.get('window');
+
+const COLORS = {
+  primary: "#008F32",
+  secondary: "#edecee",
+
+  gray: "#333333",
+  gray2: "#525252",
+
+  offwhite: "#F8F8FA",
+  blue:"#2781EB",
+  white: "#FFFFFF",
+  black: "#000000",
+  red: "#F14141",
+  lightWhite: "#FAFAFC",
+};
+
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 44,
+  height,
+  width
+};
+
+
+const SHADOWS = {
+  small: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+  medium: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5.84,
+    elevation: 5,
+  },
+};
+
+
+export { COLORS, SIZES , SHADOWS };
