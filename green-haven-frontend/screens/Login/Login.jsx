1a3e89a144c79c13b9c6332cf5e075de06a3f506
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-29 18:40:55 +0200

    Add Email Input + Validation

--- green-haven-frontend/screens/Login/Login.jsx
+++ green-haven-frontend/screens/Login/Login.jsx
@@ -22,8 +22,56 @@ const Login = () => {
 
   return (
     <SafeAreaView>
-      <Image source={require("../../assets/images/signinImage.png")} style={styles.image} />
-      
+      <Image
+        source={require("../../assets/images/signinImage.png")}
+        style={styles.image}
+      />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          isValid,
+          setFieldTouched,
+          touched,
+        }) => (
+          <View>
+            <View style={styles.inputs}>
+              <Text style={styles.label}>Email</Text>
+              <View
+                style={styles.inputWrapper(
+                  touched.email ? COLORS.primary : COLORS.white
+                )}
+              >
+                <MaterialCommunityIcons
+                  name="email-outline"
+                  size={25}
+                  color={COLORS.gray}
+                />
+                <TextInput
+                  placeholder="Enter Your Email"
+                  onFocus={() => setFieldTouched("email")}
+                  onBlur={() => setFieldTouched("email", "")}
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  value={values.email}
+                  onChangeText={handleChange("email")}
+                  style={styles.input}
+                />
+              </View>
+              {touched.email && errors.email && (
+                <Text style={styles.errorMsg}>{errors.email}</Text>
+              )}
+            </View>
+          </View>
+        )}
+      </Formik>
     </SafeAreaView>
   );
 };
--- green-haven-frontend/screens/Login/login.styles.js
+++ green-haven-frontend/screens/Login/login.styles.js
@@ -8,6 +8,37 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
     marginBottom: SIZES.xLarge,
   },
+  inputs: {
+    marginBottom: 20,
+    marginHorizontal: 20,
+  },
+  label: {
+    fontFamily: "regular",
+    marginBottom: 5,
+    marginEnd: 5,
+    fontSize: SIZES.xSmall,
+  },
+  inputWrapper: (borderColor) => ({
+    borderColor: borderColor,
+    backgroundColor: COLORS.lightWhite,
+    borderWidth: 1,
+    height: 50,
+    borderRadius: 12,
+    flexDirection: "row",
+    paddingHorizontal: 15,
+    alignItems: "center",
+  }),
+  input: {
+    flex: 1,
+    marginLeft: SIZES.medium
+  },
+  errorMsg: {
+    color: COLORS.red,
+    fontFamily: "regular",
+    marginTop: 5,
+    marginLeft: 5,
+    fontSize: SIZES.xSmall,
+  },
 });
 
 export default styles;
