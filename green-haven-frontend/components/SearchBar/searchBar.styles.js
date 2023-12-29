c4ed7d08e3b790a815ad6bca1ae49a3a2ce27b47
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-30 00:46:45 +0200

    Add SearchBar Styles

---
+++ green-haven-frontend/components/SearchBar/searchBar.styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../assets/constants";
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.medium,
+    marginVertical: SIZES.medium,
+    height: 50,
+    marginHorizontal: SIZES.small,
+  },
+  searchIcon: {
+    marginHorizontal: 10,
+    color: COLORS.gray,
+  },
+  searchWrapper: {
+    flex: 1,
+    backgroundColor: COLORS.secondary,
+    marginRight: SIZES.small,
+    borderRadius: SIZES.small,
+  },
+  searchInput: {
+    fontFamily: "regular",
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: SIZES.small,
+  },
+  searchBtn: {
+    width: 50,
+    height: "100%",
+    backgroundColor: COLORS.primary,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: SIZES.small,
+  },
+});
+
+export default styles
