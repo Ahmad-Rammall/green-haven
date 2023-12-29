a9639590e247a72431e85afdb46c2abad5ec5327
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-29 16:16:20 +0200

    Create Screens Files

---
+++ green-haven-frontend/screens/Garden/Garden.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Garden = () => {
+  return (
+    <View>
+      <Text>Garden</Text>
+    </View>
+  )
+}
+
+export default Garden
+
+const styles = StyleSheet.create({})
---
+++ green-haven-frontend/screens/Market/Market.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Market = () => {
+  return (
+    <View>
+      <Text>Market</Text>
+    </View>
+  )
+}
+
+export default Market
+
+const styles = StyleSheet.create({})
