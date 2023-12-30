96ab12d82617e69f2bf874de1fe77626181a76a3
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-30 15:41:44 +0200

    Add Cart Item

--- green-haven-frontend/components/CartItem/CartItem.jsx
+++ green-haven-frontend/components/CartItem/CartItem.jsx
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import styles from "./cartItem.styles";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { SIZES } from "../../assets/constants";
 
 const CartItem = () => {
   return (
-    <View>
-      <Text>CartItem</Text>
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/Carousel/plant1.jpg")}
+        />
+      </View>
+
+      <View style={styles.rightContainer}>
+        <View>
+          <Text style={styles.text("bold", SIZES.large)}>Plant Name</Text>
+          <Text style={styles.text("semibold", SIZES.medium)}>10 $</Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.quantity}>
+            <TouchableOpacity>
+              <Ionicons
+                name="ios-remove"
+                size={24}
+                style={[styles.quantityBtn, styles.minusBtn]}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontSize: 18, marginHorizontal: 10 }}>1</Text>
+            <TouchableOpacity>
+              <Ionicons
+                name="add"
+                size={24}
+                style={[styles.quantityBtn, styles.plusBtn]}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.delete}>
+            <TouchableOpacity>
+              <Ionicons
+                name="trash-outline"
+                size={24}
+                style={styles.deleteBtn}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default CartItem
-
-const styles = StyleSheet.create({})
+export default CartItem;
