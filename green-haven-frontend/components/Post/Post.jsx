61681d20c6c2a03f5fa00bcc51374b2723160eee
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-01 14:29:25 +0200

    Create Post Component Files

---
+++ green-haven-frontend/components/Post/Post.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import styles from './post.styles'
+import React from 'react'
+
+const Post = () => {
+  return (
+    <View>
+      <Text>Post</Text>
+    </View>
+  )
+}
+
+export default Post
