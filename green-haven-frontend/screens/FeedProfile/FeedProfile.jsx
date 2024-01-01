f269e5cd85d66a9875878cb9777615579bd5d3e2
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-01 23:01:46 +0200

    Add Top Container User Profile

--- green-haven-frontend/screens/FeedProfile/FeedProfile.jsx
+++ green-haven-frontend/screens/FeedProfile/FeedProfile.jsx
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import styles from "./feedPofile.styles";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { COLORS } from "../../assets/constants";
+import { Post } from "../../components";
 
 const FeedProfile = () => {
+  const route = useRoute();
+  const { userImage, userName, description, postImage } = route.params;
+
   return (
-    <View>
-      <Text>FeedProfile</Text>
+    <View style={styles.container}>
+      <ScrollView style={styles.wrapper}>
+        <View style={styles.topContainer}>
+          <Image source={userImage} style={styles.image} />
+          <View>
+            <Text style={styles.userName}>{userName}</Text>
+            <View style={styles.buttons}>
+              {/* Follow/Unfollow Button */}
+              <TouchableOpacity style={[styles.followBtn, styles.btn]}>
+                <Text style={styles.followTxt}>Follow</Text>
+              </TouchableOpacity>
+
+              {/* Message Button */}
+              <TouchableOpacity style={[styles.messageBtn, styles.btn]}>
+                <Ionicons
+                  name="paper-plane-outline"
+                  size={24}
+                  color={COLORS.offwhite}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default FeedProfile
-
-const styles = StyleSheet.create({})
+export default FeedProfile;
