f1933dfed1d43c75377458a79885e92b8f40b954
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-01 22:02:46 +0200

    Add Navigation to User Profile From Feed

--- green-haven-frontend/components/Post/Post.jsx
+++ green-haven-frontend/components/Post/Post.jsx
@@ -3,36 +3,43 @@ import styles from "./post.styles";
 import { Ionicons, FontAwesome } from "@expo/vector-icons";
 import React, { useState } from "react";
 import { COLORS } from "../../assets/constants";
+import { useNavigation } from "@react-navigation/native";
 
 const Post = ({ postImage, description, userName, userImage }) => {
   const [isLiked, setIsLiked] = useState(false);
+  const navigation = useNavigation();
+
   const handleLike = () => {
     console.log("xxxxxxx");
     setIsLiked(!isLiked);
   };
+
+  const navigateToUserProfile = () => {
+    navigation.navigate("User Profile", {
+      postImage,
+      description,
+      userName,
+      userImage,
+    });
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image
-          source={postImage}
-          style={styles.postImage}
-        />
+        <Image source={postImage} style={styles.postImage} />
       </View>
-      <Text style={styles.description}>
-        {description}
-      </Text>
+      <Text style={styles.description}>{description}</Text>
 
       <View style={styles.bottomContainer}>
-        <View style={styles.profileContainer}>
-          <Image
-            source={userImage}
-            style={styles.profilePic}
-          />
+        <TouchableOpacity
+          style={styles.profileContainer}
+          onPress={navigateToUserProfile}
+        >
+          <Image source={userImage} style={styles.profilePic} />
           <View style={styles.profileText}>
             <Text style={styles.userName}>{userName}</Text>
             <Text style={styles.time}>10 min ago</Text>
           </View>
-        </View>
+        </TouchableOpacity>
         <View style={styles.buttons}>
           <View style={styles.button}>
             <Text>100</Text>
@@ -42,11 +49,7 @@ const Post = ({ postImage, description, userName, userImage }) => {
             <View style={styles.button}>
               <Text>100</Text>
               {isLiked ? (
-                <Ionicons
-                  name="heart"
-                  size={30}
-                  color={COLORS.red}
-                />
+                <Ionicons name="heart" size={30} color={COLORS.red} />
               ) : (
                 <Ionicons name="heart-outline" size={30} />
               )}
