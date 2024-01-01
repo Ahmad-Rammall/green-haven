434599aabc4aea44d5156058fbb64beea62b6335
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-01 23:02:39 +0200

    Add Feed Profile Styles

---
+++ green-haven-frontend/screens/FeedProfile/feedPofile.styles.js
@@ -0,0 +1,70 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../assets/constants";
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: COLORS.offwhite,
+    },
+    wrapper:{
+        margin: SIZES.small,
+    },
+    topContainer:{
+        flexDirection: "row",
+        justifyContent:"space-evenly",
+        alignItems: "center",
+        marginBottom: 20
+    },
+    image:{
+        width: 100,
+        height: 100,
+        resizeMode: "cover"
+    },
+    buttons:{
+        flexDirection: "row",
+        gap: 10,
+    },
+    userName:{
+        alignSelf: "center",
+        marginBottom: 5,
+        fontFamily: "bold",
+        fontSize: SIZES.medium
+    },
+    btn:{
+        padding: 5,
+        borderRadius: 10,
+        alignItems: "center"
+    },
+    followBtn:{
+        backgroundColor: COLORS.primary,
+        width: 80,
+    },
+    followTxt:{
+        fontFamily:"bold",
+        color: COLORS.offwhite
+    },
+    messageBtn:{
+        backgroundColor: COLORS.blue,
+        width: 50,
+    },
+    bottomContainer:{
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginBottom: 20,
+        alignItems: "center"
+    },
+    bottomElement:{
+        alignItems: "center"
+    },
+    elementNumber:{
+        fontFamily: "bold"
+    },
+    elementName:{
+        fontFamily: "light",
+        color: COLORS.gray,
+        fontSize: 12
+    }
+
+});
+
+export default styles
