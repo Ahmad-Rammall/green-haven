32335eda0456875484a5c2eed81f049576e7a7d1
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-01 21:48:37 +0200

    Add Chat Button Feed Header

--- green-haven-frontend/navigators/FeedStack.jsx
+++ green-haven-frontend/navigators/FeedStack.jsx
@@ -2,10 +2,11 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Ionicons } from "@expo/vector-icons";
 import { Feed, FeedProfile, Chat, Conversation } from "../screens";
 import { COLORS } from "../assets/constants";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 
 const FeedStack = createNativeStackNavigator();
 
-const screenOptions = ({ route }) => ({
+const screenOptions = ({ route, navigation }) => ({
   headerShown: true,
   headerStyle: {
     backgroundColor: COLORS.primary,
@@ -20,6 +21,22 @@ const screenOptions = ({ route }) => ({
   headerBackImage: ({ tintColor }) => (
     <Ionicons name="ios-arrow-back" size={24} color={tintColor} />
   ),
+  // Cart Icon On The Right
+  headerRight: () => (
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Chat");
+        }}
+      >
+        <Ionicons
+          name="ios-chatbubbles-outline"
+          size={24}
+          color={COLORS.offwhite}
+        />
+      </TouchableOpacity>
+    </View>
+  ),
 });
 
 // Navigations inside the market section
@@ -28,7 +45,11 @@ const FeedStackNavigator = () => {
     <FeedStack.Navigator screenOptions={screenOptions}>
       <FeedStack.Screen name="Feed" component={Feed} />
       <FeedStack.Screen name="User Profile" component={FeedProfile} />
-      <FeedStack.Screen name="Chat" component={Chat} />
+      <FeedStack.Screen
+        name="Chat"
+        component={Chat}
+        options={{ headerRight: () => <View /> }}
+      />
       <FeedStack.Screen name="Conversation" component={Conversation} />
     </FeedStack.Navigator>
   );
