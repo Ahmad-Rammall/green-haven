aa54f0048a2894ab966345a27d4d6a018ee41fd9
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-01 16:10:04 +0200

    Add Post Styles

---
+++ green-haven-frontend/components/Post/post.styles.js
@@ -0,0 +1,59 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../assets/constants";
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: COLORS.secondary,
+        borderRadius: 5,
+        padding: 10,
+    },
+    imageContainer:{
+        width: "100%",
+    },
+    postImage:{
+        resizeMode: "cover",
+        width: "100%",
+        borderRadius: 5,
+    },
+    description:{
+        fontFamily: "regular",
+        fontSize: SIZES.small
+    },
+    bottomContainer:{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    profileContainer:{
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 7,
+    },
+    profilePic:{
+        width: 50,
+        height: 50,
+        resizeMode: "contain"
+    },
+    userName: {
+        fontFamily: "semibold",
+        fontSize: SIZES.small,
+    },
+    time:{
+        fontFamily: "light",
+        fontSize: SIZES.xSmall,
+        color: COLORS.gray
+    },
+    buttons:{
+        flexDirection: "row",
+        gap: 10,
+        alignItems: "center",
+    },
+    button:{
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5,
+    }
+
+});
+
+export default styles
