74e05a88983c2f43906a4b997a9e934e94b7c5af
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-01 12:46:25 +0200

    Add Change Password Screen

--- green-haven-frontend/components/Button/Button.jsx
+++ green-haven-frontend/components/Button/Button.jsx
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     backgroundColor: color,
     padding: 5,
     borderRadius: SIZES.medium,
+    marginBottom: SIZES.small
   }),
   text: {
     color: COLORS.white,
--- green-haven-frontend/screens/ChangePassword/ChangePassword.jsx
+++ green-haven-frontend/screens/ChangePassword/ChangePassword.jsx
@@ -1,6 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Keyboard,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import * as Yup from "yup";
 import { Formik } from "formik";
+import { COLORS, SIZES } from "../../assets/constants";
+import { Button } from "../../components";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import styles from "./changePassword.styles";
 
 import React, { useState } from "react";
 
@@ -20,12 +34,150 @@ const ChangePassword = () => {
   });
 
   return (
-    <View>
-      <Text>ChangePassword</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView onPress={Keyboard.dismiss()} style={styles.wrapper} showsVerticalScrollIndicator={false}>
+        <Image
+          source={require("../../assets/images/lock.png")}
+          style={styles.image}
+        />
+        <Text style={styles.notes}>
+          When changing your password, ensure it meets security standards by
+          having a minimum length of eight characters, including a mix of
+          uppercase and lowercase letters, numbers, and special characters.
+          Avoid using easily guessable information.
+        </Text>
+        <Formik
+          initialValues={{
+            oldPassword: "",
+            newPassword: "",
+            confirmNewPassword: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            isValid,
+            setFieldTouched,
+            touched,
+          }) => (
+            <View>
+              {/* Old Password */}
+
+              <View style={styles.inputs}>
+                <Text style={styles.label}>Old Password</Text>
+                <View
+                  style={styles.inputWrapper(
+                    touched.oldPassword ? COLORS.primary : COLORS.white
+                  )}
+                >
+                  <MaterialCommunityIcons
+                    name="lock-outline"
+                    size={25}
+                    color={COLORS.gray}
+                  />
+                  <TextInput
+                    placeholder="Old Password"
+                    onFocus={() => setFieldTouched("oldPassword")}
+                    onBlur={() => setFieldTouched("oldPassword", "")}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={values.oldPassword}
+                    onChangeText={handleChange("oldPassword")}
+                    secureTextEntry={secureText}
+                    style={styles.input}
+                  />
+                  <TouchableOpacity onPress={() => setSecureText(!secureText)}>
+                    <MaterialCommunityIcons
+                      name={secureText ? "eye-outline" : "eye-off-outline"}
+                      size={18}
+                    />
+                  </TouchableOpacity>
+                </View>
+                {touched.oldPassword && errors.oldPassword && (
+                  <Text style={styles.errorMsg}>{errors.oldPassword}</Text>
+                )}
+              </View>
+
+              {/* New Password */}
+
+              <View style={styles.inputs}>
+                <Text style={styles.label}>New Password</Text>
+                <View
+                  style={styles.inputWrapper(
+                    touched.newPassword ? COLORS.primary : COLORS.white
+                  )}
+                >
+                  <MaterialCommunityIcons
+                    name="lock-outline"
+                    size={25}
+                    color={COLORS.gray}
+                  />
+                  <TextInput
+                    placeholder="New Password"
+                    onFocus={() => setFieldTouched("newPassword")}
+                    onBlur={() => setFieldTouched("newPassword", "")}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={values.newPassword}
+                    onChangeText={handleChange("newPassword")}
+                    secureTextEntry={secureText}
+                    style={styles.input}
+                  />
+                </View>
+                {touched.newPassword && errors.newPassword && (
+                  <Text style={styles.errorMsg}>{errors.newPassword}</Text>
+                )}
+              </View>
+
+              {/* Confirm Password */}
+
+              <View style={styles.inputs}>
+                <Text style={styles.label}>Confirm New Password</Text>
+                <View
+                  style={styles.inputWrapper(
+                    touched.confirmNewPassword ? COLORS.primary : COLORS.white
+                  )}
+                >
+                  <MaterialCommunityIcons
+                    name="lock-outline"
+                    size={25}
+                    color={COLORS.gray}
+                  />
+                  <TextInput
+                    placeholder="confirm New Password"
+                    onFocus={() => setFieldTouched("confirmNewPassword")}
+                    onBlur={() => setFieldTouched("confirmNewPassword", "")}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={values.confirmNewPassword}
+                    onChangeText={handleChange("confirmNewPassword")}
+                    secureTextEntry={secureText}
+                    style={styles.input}
+                  />
+                </View>
+                {touched.confirmNewPassword && errors.confirmNewPassword && (
+                  <Text style={styles.errorMsg}>
+                    {errors.confirmNewPassword}
+                  </Text>
+                )}
+              </View>
+              <Button
+                onPress={isValid ? handleSubmit : () => {}}
+                isValid={isValid}
+                btnText="Save"
+                color={COLORS.primary}
+              />
+            </View>
+          )}
+        </Formik>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default ChangePassword;
-
-const styles = StyleSheet.create({});
