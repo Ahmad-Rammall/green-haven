22ab0bdba1fb08c259483616ad0b8117ca057759
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-01 23:48:03 +0200

    Add Chat Screen Styles

--- green-haven-frontend/screens/Chat/Chat.jsx
+++ green-haven-frontend/screens/Chat/Chat.jsx
@@ -1,12 +1,23 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import React from "react";
 import UserChat from "../../components/UserChat/UserChat";
-import { COLORS, SIZES } from "../../assets/constants";
+import styles from "./chat.styles";
 
 const Chat = () => {
   return (
     <View style={styles.container}>
       <ScrollView style={styles.wrapper}>
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
       </ScrollView>
     </View>
   );
@@ -14,4 +25,4 @@ const Chat = () => {
 
 export default Chat;
 
-const styles = StyleSheet.create({});
+
---
+++ green-haven-frontend/screens/Chat/chat.styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../assets/constants";
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: COLORS.offwhite,
+    },
+    wrapper:{
+      margin: SIZES.small,
+    }
+  });
+
+  export default styles
