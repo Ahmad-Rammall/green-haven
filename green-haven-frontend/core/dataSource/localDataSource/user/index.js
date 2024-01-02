2841e1f67c759347407b4616b309f30a8b10e942
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-02 23:14:33 +0200

    Create User Slice

---
+++ green-haven-frontend/core/dataSource/localDataSource/user/index.js
@@ -0,0 +1,64 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: "",
+  email: "",
+  username: "",
+  profilePicture: "",
+  bio: "",
+  phoneNumber: "",
+  location: "",
+  role: "",
+  garden: [],
+  cart: [],
+  following: [],
+  followers: [],
+};
+
+export const userSliceName = "User";
+
+export const userSlice = createSlice({
+  name: userSliceName,
+  initialState,
+  reducers: {
+    loggedInUser: (state, action) => {
+      const {
+        token,
+        email,
+        username,
+        profilePicture,
+        bio,
+        phoneNumber,
+        location,
+        role,
+        garden,
+        cart,
+        following,
+        followers,
+      } = action.payload;
+
+      return {
+        ...state,
+        token,
+        email,
+        username,
+        profilePicture,
+        bio,
+        phoneNumber,
+        location,
+        role,
+        garden,
+        cart,
+        following,
+        followers,
+      };
+    },
+    cleanData: (state, action) => {
+      return { ...initialState };
+    },
+  },
+});
+
+export const { loggedInUser, cleanData } = userSlice.actions;
+
+export default userSlice.reducer;
