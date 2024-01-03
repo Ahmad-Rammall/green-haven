463df3bc36dffe797f10844355698db1dc07e64a
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-03 18:44:30 +0200

    Add UserReduces to Store

--- green-haven-frontend/core/dataSource/localDataSource/store.js
+++ green-haven-frontend/core/dataSource/localDataSource/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer, { userSliceName } from "./user";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [userSliceName] : userReducer
+  },
   devTools: true,
 });
