d295d74087d1a48aeac7bf73ce5c54ab9c57cbd2
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-04 13:24:50 +0200

    Fix Getting Public Images

--- green-haven-frontend/components/ProductCard/ProductCard.jsx
+++ green-haven-frontend/components/ProductCard/ProductCard.jsx
@@ -2,18 +2,21 @@ import { Text, TouchableOpacity, View, Image } from "react-native";
 import React from "react";
 import styles from "./productCardStyles";
 import { useNavigation } from "@react-navigation/native";
+import {PUBLIC_FOLDER} from "@env"
 
 const ProductCard = ({ product }) => {
   const navigation = useNavigation();
   const { user, image, name, description, price } = product
+  const imageUrl = PUBLIC_FOLDER + 'products-pics/' + image;
+
   const gotoProductDetails = () => {
-    navigation.navigate("Product Details", { image, name, description, price })
+    navigation.navigate("Product Details", { imageUrl, name, description, price })
   }
   return (
     <TouchableOpacity onPress={gotoProductDetails}>
       <View style={styles.container}>
         <View style={styles.imageContainer}>
-          <Image source={ image } style={styles.image} />
+          <Image source={ { uri: imageUrl } } style={styles.image} />
         </View>
         <View style={styles.details}>
           <Text style={styles.title}>{name}</Text>
