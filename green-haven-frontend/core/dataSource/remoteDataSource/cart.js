31ead5c87aa24012f78924f34fcb1aa801c8d6b3
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-04 18:08:14 +0200

    Add Cart Remote DataSource

---
+++ green-haven-frontend/core/dataSource/remoteDataSource/cart.js
@@ -0,0 +1,13 @@
+import { sendRequest } from "../../helpers/request";
+
+export const cartDataSource = {
+  addProductToCart: async (data) => {
+    const response = await sendRequest({
+      route: "cart",
+      method: "POST",
+      body: data
+    });
+
+    return response;
+  },
+};
