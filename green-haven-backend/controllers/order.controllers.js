a515e987d4927aed0b225f520a36cf386386faa2
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-09 19:06:00 +0200

    Backend <> Add CreateOrder API

--- green-haven-backend/controllers/order.controllers.js
+++ green-haven-backend/controllers/order.controllers.js
@@ -1,11 +1,41 @@
 const User = require("../models/user.model");
 const Product = require("../models/product.model");
+const Order = require("../models/order.model");
 
-const createOrder = async (req,res) => {};
+const createOrder = async (req, res) => {
+  const { productId, quantity, location } = req.body;
 
-const deleteOrder = async (req,res) => {};
+  try {
+    // get product from database
+    const product = await Product.findOne({ _id: productId }).populate("user");
+
+    if (!product) {
+      res.status(404).json({ message: "Product Doesn't Exist" });
+    }
+
+    const totalAmount = product.price * quantity;
+
+    // create new order
+    const order = new Order({
+      client: req.user,
+      product,
+      location,
+      totalAmount,
+      quantity,
+    });
+
+    // save order
+    await order.save({ new: true, runValidators: true });
+
+    res.status(200).json({ message: "Order Created", order });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Error" });
+  }
+};
+
+const deleteOrder = async (req, res) => {};
 
 module.exports = {
-    createOrder,
-    deleteOrder,
-}
+  createOrder,
+  deleteOrder,
+};
