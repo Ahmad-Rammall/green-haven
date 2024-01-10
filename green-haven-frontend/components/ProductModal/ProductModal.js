d192e06e6dd926a3cdb33f3bf17e04aea8a3ebd6
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-10 21:33:53 +0200

    Frontend <> Create ProductModal Validation

--- green-haven-frontend/components/ProductModal/ProductModal.js
+++ green-haven-frontend/components/ProductModal/ProductModal.js
@@ -1,7 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { COLORS } from "../../assets/constants";
+import styles from "./productModal.styles";
 
 const ProductModal = () => {
+    const validationSchema = Yup.object({
+        description: Yup.string()
+          .max(500, "Max 500 characters")
+          .required("Required"),
+        name: Yup.string().max(50, "Max 50 Characters").required("Required"), // Fixed the typo and added the missing parenthesis
+        price: Yup.number()
+          .required("Price is required")
+          .positive("Number must be positive")
+          .min(1, "Price must be at least $ 1"),
+      });
   return (
     <View>
       <Text>ProductModal</Text>
@@ -11,4 +25,3 @@ const ProductModal = () => {
 
 export default ProductModal
 
-const styles = StyleSheet.create({})
