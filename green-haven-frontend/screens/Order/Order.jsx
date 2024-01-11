994b15a66ba7336b80db53f2755aa8e42fd21c4d
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-11 20:34:40 +0200

    Frontend <> Add Order Item to Order Screen

--- green-haven-frontend/screens/Order/Order.jsx
+++ green-haven-frontend/screens/Order/Order.jsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { OrderItem } from "../../components";
+import { COLORS } from "../../assets/constants";
 
 const Order = () => {
   return (
-    <View>
-      <Text>Order</Text>
+    <View style={{ backgroundColor: COLORS.offwhite, flex:1 }}>
+      <OrderItem />
     </View>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
