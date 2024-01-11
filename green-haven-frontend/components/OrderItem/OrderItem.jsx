c587323915b7a4c750b714773acaaed747e8d7a8
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-11 20:33:58 +0200

    Frontend <> Build Order Item

--- green-haven-frontend/components/OrderItem/OrderItem.jsx
+++ green-haven-frontend/components/OrderItem/OrderItem.jsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import styles from "./orderItem.styles";
 
 const OrderItem = () => {
+  const Detail = ({ title, detail }) => {
+    return (
+      <View style={styles.detailContainer}>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{title}</Text>
+          <Text> : </Text>
+        </View>
+        <Text style={styles.detail}>{detail}</Text>
+      </View>
+    );
+  };
   return (
-    <View>
-      <Text>OrderItem</Text>
+    <View style={styles.container}>
+      <Text style={styles.orderId}>Order Id</Text>
+      <Detail title="Client Name" detail={"xxxxxxxxxxxxxxxxxxxx"} />
+      <Detail title="Client Phone" detail={"xxxxxxxxxxxxxxxxxxxx"} />
+      <Detail title="Location" detail={"xxxxxxxxxxxxxxxxxxxx"} />
+      <Detail title="Quantity" detail={"xxxxxxxxxxxxxxxxxxxx"} />
+      <Text style={styles.line}></Text>
+      <Detail title="Total Amount" detail={"$ 10"} />
     </View>
-  )
-}
+  );
+};
 
-export default OrderItem
-
-const styles = StyleSheet.create({})
+export default OrderItem;
