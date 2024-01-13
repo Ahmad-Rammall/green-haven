eac5c97c2bca1a2a442ab93370ddb60fcc7d8626
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-13 15:18:06 +0200

    Frontend <> Fetch GetPosts

--- green-haven-frontend/screens/Feed/Feed.jsx
+++ green-haven-frontend/screens/Feed/Feed.jsx
@@ -1,11 +1,22 @@
 import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
 import { Post, SearchBar, Story } from "../../components";
 import styles from "./feed.styles";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { postDataSource } from "../../core/dataSource/remoteDataSource/post";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
   const desc =
     "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae soluta ratione excepturi. Ipsum id harum consectetur necessitatibus, in dolorequis eos vero quam totam, voluptate, deserunt rerum ullam repudiandaeneque assumenda doloremque explicabo? Incidunt modi non reprehenderitullam ut quis!";
+
+  const getPosts = async () => {
+    const response = await postDataSource.getPosts();
+    setPosts(response.data[0]);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -42,24 +53,10 @@ const Feed = () => {
         </ScrollView>
         <View style={styles.postsWrapper}>
           <SearchBar placeholder={"Search For Users"} />
-          <Post
-            userImage={require("../../assets/images/noUserImage.png")}
-            postImage={require("../../assets/images/Carousel/plant1.jpg")}
-            description={desc}
-            userName="Ahmad"
-          />
-          <Post
-            userImage={require("../../assets/images/noUserImage.png")}
-            postImage={require("../../assets/images/Carousel/plant2.jpg")}
-            description={desc}
-            userName="Ahmad"
-          />
-          <Post
-            userImage={require("../../assets/images/noUserImage.png")}
-            postImage={require("../../assets/images/Carousel/plant3.jpg")}
-            description={desc}
-            userName="Ahmad"
-          />
+
+          {posts.map((post) => (
+            <Post key={post._id} post={post} />
+          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
