cabb797f1f8802978e555a21273f5a8c69608101
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-13 15:47:42 +0200

    Frontend <> Update Post Component

--- green-haven-frontend/components/Post/Post.jsx
+++ green-haven-frontend/components/Post/Post.jsx
@@ -4,10 +4,16 @@ import { Ionicons, FontAwesome } from "@expo/vector-icons";
 import React, { useState } from "react";
 import { COLORS } from "../../assets/constants";
 import { useNavigation } from "@react-navigation/native";
+import {PUBLIC_FOLDER} from "@env";
+import {format} from "timeago.js"
 
-const Post = ({ postImage, description, userName, userImage }) => {
+const Post = ({ description, userName, userImage, post }) => {
   const [isLiked, setIsLiked] = useState(false);
   const navigation = useNavigation();
+
+  const postImage = PUBLIC_FOLDER + "posts-pics/" + post.image
+  const userProfilePic = PUBLIC_FOLDER + "profile-pics/" + post.user.profile_picture
+
 
   const handleLike = () => {
     console.log("xxxxxxx");
@@ -25,29 +31,29 @@ const Post = ({ postImage, description, userName, userImage }) => {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={postImage} style={styles.postImage} />
+        <Image source={{ uri: postImage }} style={styles.postImage} />
       </View>
-      <Text style={styles.description}>{description}</Text>
+      <Text style={styles.description}>{post.description}</Text>
 
       <View style={styles.bottomContainer}>
         <TouchableOpacity
           style={styles.profileContainer}
           onPress={navigateToUserProfile}
         >
-          <Image source={userImage} style={styles.profilePic} />
+          <Image source={{ uri: userProfilePic }} style={styles.profilePic} />
           <View style={styles.profileText}>
-            <Text style={styles.userName}>{userName}</Text>
-            <Text style={styles.time}>10 min ago</Text>
+            <Text style={styles.userName}>{post.user.name}</Text>
+            <Text style={styles.time}>{format(post.createdAt)}</Text>
           </View>
         </TouchableOpacity>
         <View style={styles.buttons}>
           <View style={styles.button}>
-            <Text>100</Text>
+            <Text>{post.comments.length}</Text>
             <FontAwesome name="comment-o" size={30} />
           </View>
           <TouchableOpacity onPress={() => handleLike()}>
             <View style={styles.button}>
-              <Text>100</Text>
+              <Text>{post.likes.length}</Text>
               {isLiked ? (
                 <Ionicons name="heart" size={30} color={COLORS.red} />
               ) : (
