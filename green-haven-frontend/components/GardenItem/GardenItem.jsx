684e5d1b1e2f687b7814e4af9fe4e8baddef3188
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-14 00:30:29 +0200

    Frontend <> Update GardenItem Component

--- green-haven-frontend/components/GardenItem/GardenItem.jsx
+++ green-haven-frontend/components/GardenItem/GardenItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Image, TouchableOpacity } from "react-native";
 import styles from "./gardenItem.styles";
+import {PUBLIC_FOLDER} from "@env"
 
-const GardenItem = ({ imageSource }) => {
+const GardenItem = ({ plant }) => {
+  console.log(PUBLIC_FOLDER)
+  const image = PUBLIC_FOLDER + "garden-pics/"+ plant.plant_picture
   return (
     <TouchableOpacity style={styles.container}>
-      <Image source={imageSource} style={styles.image} />
+      <Image source={{ uri:image }} style={styles.image} />
     </TouchableOpacity>
   );
 };
--- green-haven-frontend/screens/Garden/Garden.jsx
+++ green-haven-frontend/screens/Garden/Garden.jsx
@@ -1,21 +1,28 @@
 import { StyleSheet, Text, View, ScrollView } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GardenItem } from "../../components";
 import styles from "./garden.styles";
+import { gardenDataSource } from "../../core/dataSource/remoteDataSource/garden";
 
 const Garden = () => {
+  const [plants, setPlants] = useState([]);
+
+  const getPlants = async () => {
+    const response = await gardenDataSource.getAllPlants();
+    if (response?.status === 200) {
+      setPlants(response.data.garden);
+    }
+  };
+
+  useEffect(() => {
+    getPlants();
+  }, []);
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
       <View style={styles.wrapper}>
-        <GardenItem imageSource={require("../../assets/images/plant5.jpg")} />
-        <GardenItem imageSource={require("../../assets/images/plant5.jpg")} />
-        <GardenItem imageSource={require("../../assets/images/plant5.jpg")} />
-        <GardenItem imageSource={require("../../assets/images/plant5.jpg")} />
-        <GardenItem imageSource={require("../../assets/images/plant5.jpg")} />
-        <GardenItem imageSource={require("../../assets/images/plant5.jpg")} />
-        <GardenItem imageSource={require("../../assets/images/plant5.jpg")} />
-        <GardenItem imageSource={require("../../assets/images/plant5.jpg")} />
-
+        {plants.map((plant) => (
+          <GardenItem key={plant._id} plant={plant} />
+        ))}
       </View>
     </ScrollView>
   );
