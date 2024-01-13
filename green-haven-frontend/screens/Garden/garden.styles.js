dce60fbb21220a14aaca654bf58a18c4166cb4a5
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-14 01:05:10 +0200

    Frontend <> Add BottomSheet Modal Styles

--- green-haven-frontend/screens/Garden/garden.styles.js
+++ green-haven-frontend/screens/Garden/garden.styles.js
@@ -2,16 +2,27 @@ import { StyleSheet } from "react-native";
 import { COLORS, SIZES } from "../../assets/constants";
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor: COLORS.offwhite,
-    },
-    wrapper:{
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-        margin: SIZES.medium,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.offwhite,
+  },
+  wrapper: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    margin: SIZES.medium,
+  },
+  modalClose: {
+    display: "none",
+  },
+  modalOpen: {
+    display: "flex",
+    position: "absolute",
+    bottom: 0,
+    width: SIZES.width,
+    height: 1100,
+    zIndex: 9999,
+  },
+});
 
 export default styles;
