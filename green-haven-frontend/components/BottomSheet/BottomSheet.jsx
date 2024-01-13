7fff83dae957e3d88283e66f7aede0d3076f3242
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-14 01:05:45 +0200

    Frontend <> Update BottomSheet Modal

--- green-haven-frontend/components/BottomSheet/BottomSheet.jsx
+++ green-haven-frontend/components/BottomSheet/BottomSheet.jsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { TouchableOpacity } from "@gorhom/bottom-sheet";
 import styles from "./sheet.styles";
-const BottomSheet = ({ plant }) => {
+const BottomSheet = ({ plant, garden }) => {
   const snapPoints = ["70%"];
 
   return (
@@ -14,23 +14,25 @@ const BottomSheet = ({ plant }) => {
         </View>
       ) : (
         <ScrollView>
-          <Text style={styles.title}>{plant.name}</Text>
+          <Text style={styles.title}>{plant.plant_name}</Text>
           <View style={styles.descriptionContainer}>
             <Text style={styles.descTitle}>About the Plant :</Text>
-            <Text style={styles.description}>{plant.description}</Text>
+            <Text style={styles.description}>{plant.plant_description}</Text>
           </View>
 
-          <View style={styles.buttons}>
-            <TouchableOpacity style={styles.optionBtn}>
-              <Text style={styles.optText}> + Garden</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.optionBtn}>
-              <Text style={styles.optText}>Post</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.optionBtn}>
-              <Text style={styles.optText}>Send to</Text>
-            </TouchableOpacity>
-          </View>
+          {garden ?? (
+            <View style={styles.buttons}>
+              <TouchableOpacity style={styles.optionBtn}>
+                <Text style={styles.optText}> + Garden</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.optionBtn}>
+                <Text style={styles.optText}>Post</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.optionBtn}>
+                <Text style={styles.optText}>Send to</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         </ScrollView>
       )}
     </ScrollView>
