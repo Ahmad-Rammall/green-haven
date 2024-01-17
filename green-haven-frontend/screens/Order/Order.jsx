adcc2f0870c0908e8a7f85b7c0d6351e3681fa07
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-18 01:18:01 +0200

    Frontend <> Fetch Order Screen With Loading Modal

--- green-haven-frontend/screens/Order/Order.jsx
+++ green-haven-frontend/screens/Order/Order.jsx
@@ -1,25 +1,30 @@
 import { View, ScrollView, Text } from "react-native";
 import React, { useEffect, useState } from "react";
-import { OrderItem } from "../../components";
+import { OrderItem, LoadingModal } from "../../components";
 import styles from "./order.styles";
 import { orderDataSource } from "../../core/dataSource/remoteDataSource/order";
 
 const Order = () => {
   const [orders, setOrders] = useState([]);
   const [refresh, setRefresh] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const getOrders = async () => {
     try {
+      setLoading(true);
       const response = await orderDataSource.getAllSellerOrders();
-      setOrders(response.data.orders);
+      if (response?.status === 200) {
+        setOrders(response.data.orders);
+        setLoading(false);
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   const refreshPage = () => {
-    setRefresh(!refresh)
-  }
+    setRefresh(!refresh);
+  };
 
   useEffect(() => {
     getOrders();
@@ -27,16 +32,25 @@ const Order = () => {
 
   return (
     <View style={styles.container}>
-      {orders?.length === 0 ? (
-        <View style={styles.emptyOrders}>
-          <Text style={styles.emptyOrdersText}>No Orders</Text>
-        </View>
+      {loading ? (
+        <LoadingModal />
       ) : (
-        <ScrollView style={styles.wrapper} showsVerticalScrollIndicator={false}>
-          {orders.map((order) => (
-            <OrderItem order={order} refresh={refreshPage} />
-          ))}
-        </ScrollView>
+        <>
+          {orders?.length === 0 ? (
+            <View style={styles.emptyOrders}>
+              <Text style={styles.emptyOrdersText}>No Orders</Text>
+            </View>
+          ) : (
+            <ScrollView
+              style={styles.wrapper}
+              showsVerticalScrollIndicator={false}
+            >
+              {orders.map((order) => (
+                <OrderItem key={order._id} order={order} refresh={refreshPage} />
+              ))}
+            </ScrollView>
+          )}
+        </>
       )}
     </View>
   );
