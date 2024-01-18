2ca165ecdf7488a164278b4ed527d81228d68aa5
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-18 15:32:01 +0200

    Frontend <> Add Garden/Chatbot Buttons

--- green-haven-frontend/screens/Home/Home.jsx
+++ green-haven-frontend/screens/Home/Home.jsx
@@ -1,16 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Garden from "../Garden/Garden"
-import styles from './home.styles'
+import { TouchableOpacity, Text, View } from "react-native";
+import React from "react";
+import Garden from "../Garden/Garden";
+import styles from "./home.styles";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Garden</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.disabled]}>
+          <Text style={styles.buttonText}>ChatBot</Text>
+        </TouchableOpacity>
+      </View>
+
       <Garden />
     </View>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
