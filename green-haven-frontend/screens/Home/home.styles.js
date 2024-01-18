1b1e1a60c2374118f7a32a31cbd21358905fb238
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-18 15:32:12 +0200

    Frontend <> Add Garden/Chatbot Buttons Styles

--- green-haven-frontend/screens/Home/home.styles.js
+++ green-haven-frontend/screens/Home/home.styles.js
@@ -1,9 +1,32 @@
 import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../assets/constants";
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.offwhite
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    gap: 10,
+    marginTop: SIZES.large,
+    justifyContent: "center"
+  },
+  button: {
+    backgroundColor: COLORS.primary,
+    padding: 10,
+    width: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius:10,
+  },
+  buttonText:{
+    color: COLORS.offwhite,
+    fontFamily: "semibold",
+  },
+  disabled:{
+    backgroundColor: COLORS.gray2,
+  }
+});
 
 export default styles;
