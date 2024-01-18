f5748b86ac9e019208d8cfee322dc361073291e7
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-18 18:21:07 +0200

    Frontend <> Fetch ChatBot Messages

--- green-haven-frontend/screens/ChatBot/ChatBot.jsx
+++ green-haven-frontend/screens/ChatBot/ChatBot.jsx
@@ -1,25 +1,32 @@
 import React, { useCallback } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
+import { getGeneratedMessage } from "../../core/dataSource/remoteDataSource/chatBot";
 
 const ChatBot = ({ messages, setMessages }) => {
-  const onSend = useCallback((messages) => {
+  const onSend = useCallback(async (messages) => {
+    // get user's message
+    const userMessage = messages[0].text;
+
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, messages)
     );
+
+    // send to openAI and get response message
+    const response = await getGeneratedMessage(userMessage);
+
+    // save bot message
     const botMessage = {
       _id: new Date().getTime(),
-      text: "xxx",
+      text: `${response.data.choices[0].message.content}`,
       user: {
         _id: 1,
         name: "ChatBot",
       },
     };
 
-    setTimeout(() => {
-      setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, botMessage)
-      );
-    }, 1000);
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, botMessage)
+    );
   }, []);
 
   return (
