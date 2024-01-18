4d06bf3575e4ea94cf3c94cbd86a9e70036a61cd
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-18 15:39:17 +0200

    Frontend <> Add Garden/Chatbot Buttons Functionalities

---
+++ green-haven-frontend/screens/ChatBot/ChatBot.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChatBot = () => {
+  return (
+    <View>
+      <Text>ChatBot</Text>
+    </View>
+  )
+}
+
+export default ChatBot
+
+const styles = StyleSheet.create({})
--- green-haven-frontend/screens/Home/Home.jsx
+++ green-haven-frontend/screens/Home/Home.jsx
@@ -1,21 +1,33 @@
 import { TouchableOpacity, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Garden from "../Garden/Garden";
+import ChatBot from "../ChatBot/ChatBot";
 import styles from "./home.styles";
 
 const Home = () => {
+  const [gardenScreen, setGardenScreen] = useState(true);
   return (
     <View style={styles.container}>
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={
+            gardenScreen ? styles.button : [styles.button, styles.disabled]
+          }
+          onPress={() => setGardenScreen(true)}
+        >
           <Text style={styles.buttonText}>Garden</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.disabled]}>
+        <TouchableOpacity
+          style={
+            !gardenScreen ? styles.button : [styles.button, styles.disabled]
+          }
+          onPress={() => setGardenScreen(false)}
+        >
           <Text style={styles.buttonText}>ChatBot</Text>
         </TouchableOpacity>
       </View>
 
-      <Garden />
+      {gardenScreen ? <Garden /> : <ChatBot />}
     </View>
   );
 };
