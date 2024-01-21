d601e20a5ac936e6e6f47e94c7862eb7ddb61585
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-21 20:30:04 +0200

    Backend <> Create Get Users/Orders Counts API

---
+++ green-haven-backend/controllers/admin.controllers.js
@@ -0,0 +1,40 @@
+const User = require("../models/user.model");
+const Order = require("../models/order.model");
+
+const getCounts = async (req, res) => {
+  try {
+    const user_result = await User.aggregate([
+      {
+        $group: {
+          _id: "$role",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    if (!user_result) {
+      return res.status(400).json("error");
+    }
+
+    const orders_result = await Order.aggregate([
+        {
+          $group: {
+            _id: null,
+            count: { $sum: 1 },
+          },
+        },
+      ]);
+  
+      if (!orders_result) {
+        return res.status(400).json("error");
+      }
+
+    return res.status(200).json({user_result, orders_result});
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  getCounts,
+};
