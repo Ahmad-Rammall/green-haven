7be95dc2fe1c9993a0bf1ef62f89921b08306618
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-21 19:04:56 +0200

    Admin <> Create Redux Store

--- admin/frontend/src/main.jsx
+++ admin/frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import ThemeProvider from 'src/theme';
+import ThemeProvider from "src/theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import LoginPage from "./pages/login";
+import { Provider } from "react-redux";
+import { store } from "./core/localDataSource/store";
 
 import App from "./app";
 
@@ -12,14 +14,23 @@ import App from "./app";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <Suspense>
-        <Routes>
-          <Route path="/" element={<ThemeProvider><LoginPage /></ThemeProvider>} />
-          <Route path="/main/*" element={<App />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-  </HelmetProvider>
+  <Provider store={store}>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Suspense>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ThemeProvider>
+                  <LoginPage />
+                </ThemeProvider>
+              }
+            />
+            <Route path="/main/*" element={<App />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </HelmetProvider>
+  </Provider>
 );
