743ee1a28a61e9acc82f834975b69d956f5ce355
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-22 19:12:34 +0200

    Backend <> Create AddUser API for Admins

--- green-haven-backend/controllers/admin.controllers.js
+++ green-haven-backend/controllers/admin.controllers.js
@@ -1,7 +1,8 @@
 const User = require("../models/user.model");
 const Order = require("../models/order.model");
-const Post = require("../models/post.model")
-const Product = require("../models/product.model")
+const Post = require("../models/post.model");
+const Product = require("../models/product.model");
+const bcrypt = require("bcrypt");
 
 const getCounts = async (req, res) => {
   try {
@@ -19,85 +20,126 @@ const getCounts = async (req, res) => {
     }
 
     const orders_result = await Order.aggregate([
-        {
-          $group: {
-            _id: null,
-            count: { $sum: 1 },
-          },
+      {
+        $group: {
+          _id: null,
+          count: { $sum: 1 },
         },
-      ]);
-  
-      if (!orders_result) {
-        return res.status(400).json("error");
-      }
+      },
+    ]);
 
-    return res.status(200).json({user_result, orders_result});
+    if (!orders_result) {
+      return res.status(400).json("error");
+    }
+
+    return res.status(200).json({ user_result, orders_result });
   } catch (error) {
     return res.status(500).json(error);
   }
 };
 
-const getAllUsers = async (req,res) => {
-  try{
+const getAllUsers = async (req, res) => {
+  try {
     const users = await User.find({});
-    if(!users) return res.status(400).json({message: "error"});
+    if (!users) return res.status(400).json({ message: "error" });
     res.status(200).json(users);
-  }
-  catch(error){
+  } catch (error) {
     return res.status(500).json(error);
   }
-}
+};
 
-const getAllPosts = async (req,res) => {
-  try{
+const getAllPosts = async (req, res) => {
+  try {
     const posts = await Post.find({}).populate({
       path: "user",
       select: ["name", "profile_picture"],
     });
-    if(!posts) return res.status(400).json({message: "error"});
+    if (!posts) return res.status(400).json({ message: "error" });
     res.status(200).json(posts);
-  }
-  catch(error){
+  } catch (error) {
     return res.status(500).json(error);
   }
-}
+};
 
-const deletePost = async (req,res) => {
-  try{
+const deletePost = async (req, res) => {
+  try {
     const postId = req.params.postId;
     const isDeleted = await Post.findOneAndDelete({ _id: postId });
 
-    if(!isDeleted){
+    if (!isDeleted) {
       return res.status(400).json({ message: "Error Deleting Post" });
     }
-    console.log(isDeleted)
+    console.log(isDeleted);
     return res.status(200).json({ message: "Post Deleted" });
-  }
-  catch(error){
+  } catch (error) {
     return res.status(500).json(error);
   }
-}
+};
 
-const deleteProduct = async (req,res) => {
-  try{
+const deleteProduct = async (req, res) => {
+  try {
     const productId = req.params.productId;
     const isDeleted = await Product.findOneAndDelete({ _id: productId });
 
-    if(!isDeleted){
+    if (!isDeleted) {
       return res.status(400).json({ message: "Error Deleting Product" });
     }
-    console.log(isDeleted)
+    console.log(isDeleted);
     return res.status(200).json({ message: "Product Deleted" });
-  }
-  catch(error){
+  } catch (error) {
     return res.status(500).json(error);
   }
-}
+};
+
+const addUser = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { email, password, name, bio, phone_number, location, role, status } =
+      req.body;
+
+    if (
+      email === "" ||
+      password === "" ||
+      name === "" ||
+      role === "" ||
+      status === "" ||
+      email === undefined ||
+      password === undefined ||
+      name === undefined ||
+      role === undefined ||
+      status === undefined
+    ) {
+      return res.status(400).json({ message: "All Details required" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      role,
+      status,
+      bio,
+      phone_number,
+      location,
+    });
+
+    await user.save({ new: true, runValidators: true });
+    if (!user) return res.status(400).json({ message: "Error Adding User" });
+
+    return res.status(200).json({ message: "User Added", user });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
 module.exports = {
   getCounts,
   getAllUsers,
   getAllPosts,
   deletePost,
-  deleteProduct
+  deleteProduct,
+  addUser,
 };
--- green-haven-backend/routes/admin.routes.js
+++ green-haven-backend/routes/admin.routes.js
@@ -1,11 +1,20 @@
 const router = require("express").Router();
-const { getCounts, getAllUsers, getAllPosts, deletePost, deleteProduct } = require("../controllers/admin.controllers");
+const {
+  getCounts,
+  getAllUsers,
+  getAllPosts,
+  deletePost,
+  deleteProduct,
+  addUser,
+} = require("../controllers/admin.controllers");
 
 router.get("/", getCounts);
-router.get("/users", getAllUsers);
-router.get("/posts", getAllPosts);
-router.delete("/post/:postId", deletePost)
-router.delete("/product/:productId", deleteProduct)
 
+router.get("/users", getAllUsers);
+router.post("/users", addUser);
+
+router.get("/posts", getAllPosts);
+router.delete("/post/:postId", deletePost);
+router.delete("/product/:productId", deleteProduct);
 
 module.exports = router;
