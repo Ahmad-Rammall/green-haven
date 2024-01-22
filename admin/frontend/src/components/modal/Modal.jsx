bb08d13619a11cfe9700810233d2b9d9f2736b0f
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-22 18:32:15 +0200

    Admin <> Add User Modal Validations

--- admin/frontend/src/components/modal/Modal.jsx
+++ admin/frontend/src/components/modal/Modal.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Modal.css";
 
 const Modal = ({ isOpen, onClose }) => {
-  const [isModalOpen, setIsModalOpen] = useState(isOpen);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
-    confirmPassword: "",
+    bio: "",
     phoneNumber: "",
     address: "",
     role: "",
+    status: "",
   });
+  const [isValid, setIsValid] = useState(false);
 
   const handleClose = () => {
-    setIsModalOpen(false);
     onClose();
   };
 
@@ -23,8 +23,24 @@ const Modal = ({ isOpen, onClose }) => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  useEffect(() => {
+    if (
+      formData.name === "" ||
+      formData.email === "" ||
+      formData.password === ""
+    ) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
+  }, [formData]);
+
   const handleRoleChange = (e) => {
     setFormData((prevData) => ({ ...prevData, role: e.target.value }));
+  };
+
+  const handleStatusChange = (e) => {
+    setFormData((prevData) => ({ ...prevData, status: e.target.value }));
   };
 
   const handleSubmit = (e) => {
@@ -44,61 +60,76 @@ const Modal = ({ isOpen, onClose }) => {
             </div>
             <div className="modal-content">
               <form onSubmit={handleSubmit}>
-                <label>
+                <div className="input">
                   Name:
                   <input
                     type="text"
                     name="name"
                     value={formData.name}
                     onChange={handleInputChange}
+                    className="input-field"
                   />
-                </label>
-                <label>
+                  {!formData.name && (
+                    <span className="required-span">required</span>
+                  )}
+                </div>
+                <div className="input">
                   Email:
                   <input
                     type="email"
                     name="email"
                     value={formData.email}
                     onChange={handleInputChange}
+                    className="input-field"
                   />
-                </label>
-                <label>
+                  {!formData.email && (
+                    <span className="required-span">required</span>
+                  )}
+                </div>
+                <div className="input">
                   Password:
                   <input
                     type="password"
                     name="password"
                     value={formData.password}
                     onChange={handleInputChange}
+                    className="input-field"
                   />
-                </label>
-                <label>
+                  {!formData.password && (
+                    <span className="required-span">required</span>
+                  )}
+                </div>
+                <div className="input">
                   Phone Nb:
                   <input
-                    type="password"
-                    name="confirmPassword"
-                    value={formData.confirmPassword}
-                    onChange={handleInputChange}
-                  />
-                </label>
-                <label>
-                  Bio:
-                  <input
-                    type="tel"
+                    type="number"
                     name="phoneNumber"
                     value={formData.phoneNumber}
                     onChange={handleInputChange}
+                    className="input-field"
                   />
-                </label>
-                <label>
+                </div>
+                <div className="input">
+                  Bio:
+                  <input
+                    type="tel"
+                    name="bio"
+                    value={formData.bio}
+                    onChange={handleInputChange}
+                    className="input-field"
+                  />
+                </div>
+                <div className="input">
                   Address:
                   <input
                     type="text"
                     name="address"
                     value={formData.address}
                     onChange={handleInputChange}
+                    className="input-field"
                   />
-                </label>
-                <label>
+                </div>
+                <div className="input">
                   Role:
                   <select
                     name="role"
@@ -108,20 +139,22 @@ const Modal = ({ isOpen, onClose }) => {
                     <option value="seller">Seller</option>
                     <option value="user">User</option>
                   </select>
-                </label>
-                <label>
+                </div>
+                <div className="input">
                   Status:
                   <select
                     name="status"
                     value={formData.status}
-                    onChange={handleRoleChange}
+                    onChange={handleStatusChange}
                   >
                     <option value="active">Active</option>
                     <option value="banned">Banned</option>
                   </select>
-                </label>
+                </div>
 
-                <button type="submit">Submit</button>
+                <button type="submit" disabled={!isValid} className={!isValid ? "disabled-btn" : ""}>
+                  Submit
+                </button>
               </form>
             </div>
           </div>
