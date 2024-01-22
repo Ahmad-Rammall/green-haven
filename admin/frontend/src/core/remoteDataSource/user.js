0c54c2ef63f906ccf25678fc209c16f703a29d12
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-22 19:14:20 +0200

    Admin <> Create AddUser DataSource

--- admin/frontend/src/core/remoteDataSource/user.js
+++ admin/frontend/src/core/remoteDataSource/user.js
@@ -7,4 +7,12 @@ export const userDataSource = {
     });
     return response
   },
+  addUser: async (data) => {
+    const response = await sendRequest({
+      route: "admin/users",
+      method:"POST",
+      body: data
+    });
+    return response
+  },
 };
