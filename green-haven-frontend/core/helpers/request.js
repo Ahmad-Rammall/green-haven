bd9fb0d7de06ea795bb3aa9719c3c5098015b63c
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-24 00:27:34 +0200

    Frontend <> Show Error Messages

--- green-haven-frontend/core/helpers/request.js
+++ green-haven-frontend/core/helpers/request.js
@@ -1,30 +1,32 @@
 import axios from "axios";
 import { local } from "./localStorage";
-import { API_HOST } from "@env"
-import Toast from 'react-native-simple-toast';
+import { API_HOST } from "@env";
+import Toast from "react-native-simple-toast";
 
 export const sendRequest = async ({
   route,
   body,
   method = "GET",
-  contentType = "application/json"
+  contentType = "application/json",
 }) => {
-  const token = await local("token")
+  const token = await local("token");
 
   try {
-    process.env.API_HOST
+    process.env.API_HOST;
     const response = await axios.request({
       url: `${API_HOST}/${route}`,
       method,
       data: body,
       headers: {
         "Content-Type": contentType,
-        "Authorization": `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     });
 
     return response;
   } catch (error) {
-    Toast.show(error.message, Toast.LONG);
+    if (error.response?.data?.message)
+      Toast.show(error.response.data.message, Toast.LONG);
+    else Toast.show(error.message, Toast.LONG);
   }
 };
