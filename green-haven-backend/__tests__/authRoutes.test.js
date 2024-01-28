c3238492aa11772dbcf9fddfd1dcae4b4e39453a
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-29 01:34:25 +0200

    Backend <> Add Register Tests

--- green-haven-backend/__tests__/authRoutes.test.js
+++ green-haven-backend/__tests__/authRoutes.test.js
@@ -14,14 +14,6 @@ describe("POST /auth/login", () => {
       expect(response.body.streamToken).toBeDefined();
     });
 
-    test("Should respond with a 400 status code if email/password wrong", async () => {
-      const response = await supertest(app).post("/auth/login").send({
-        email: "user@user.com",
-        password: "000",
-      });
-      expect(response.status).toBe(400);
-    });
-
     test("Should specify a json in the header", async () => {
       const response = await supertest(app).post("/auth/login").send({
         email: "user@user.com",
@@ -32,4 +24,58 @@ describe("POST /auth/login", () => {
       );
     });
   });
+
+  describe("Wrong email or password", () => {
+    try{
+      test("Should respond with a 400 status code if email/password wrong", async () => {
+        const response = await supertest(app).post("/auth/login").send({
+          email: "user@user.com",
+          password: "000",
+        });
+        expect(response.status).toBe(400);
+      });
+    }
+    catch(error){
+      console.log(error)
+      throw error
+    }
+  });
 });
+
+describe("POST /auth/register", () => {
+  describe("Given an email, password, name, and role", () => {
+    test("Should respond with a 200 status code", async () => {
+      const response = await supertest(app).post("/auth/login").send({
+        email: "user@user.com",
+        password: "123456",
+        name: "xxx xxx",
+        role: "user",
+      });
+      expect(response.status).toBe(200);
+    });
+
+    test("Should specify a json in the header", async () => {
+      const response = await supertest(app).post("/auth/register").send({
+        email: "user@user.com",
+        password: "123456",
+        name: "xxx xxx",
+        role: "user",
+      });
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json")
+      );
+    });
+  });
+
+  describe("Required parameter not given", () => {
+    test("Should respond with a 400 status code if a parameter not given", async () => {
+      const response = await supertest(app).post("/auth/login").send({
+        email: "user@user.com",
+        password: "000",
+        role: "user"
+      });
+      expect(response.status).toBe(400);
+    });
+  });
+});
+
