4ee69e6c2385cdc47477b3cd65daa8063ca9d1c5
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-01-29 21:09:32 +0200

    Frontend <> Enhance Chatbot Prompts

--- green-haven-frontend/core/dataSource/remoteDataSource/geminiAI.js
+++ green-haven-frontend/core/dataSource/remoteDataSource/geminiAI.js
@@ -1,14 +1,13 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"
-import {GOOGLE_API} from "@env"
-GOOGLE_API
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GOOGLE_API } from "@env";
+GOOGLE_API;
 
 const genAI = new GoogleGenerativeAI(GOOGLE_API);
 
 export const postMessage = async (userMessage) => {
   try {
-    // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    
+
     const chat = model.startChat({
       history: [
         {
@@ -21,18 +20,26 @@ export const postMessage = async (userMessage) => {
         },
       ],
       generationConfig: {
-        maxOutputTokens: 300,
+        maxOutputTokens: 500,
       },
     });
     userMessage =
-      "Act as a plant enthusiast, asnwer questions related to plants only, if you are asked about a topic other than plants don't answer. Answers should be brief and clear (5 sentences). Always return a text. Topic is : " +
-      userMessage;
+      "PlantBot, as a user comes in with a variety of questions related to plants and gardening."+
+      "If the topic is not related to plants, don't answer."+
+      "Your goal is to provide accurate and informative answers. Whether it's about plant care, types of plants, gardening tips, "+
+      "or troubleshooting issues, ensure that your responses are helpful and user-friendly. "+
+      "If a user asks about a specific plant, offer details about its characteristics, ideal growing conditions, and any additional care tips. "+
+      "Always prioritize accuracy and clarity in your responses. Feel free to use botanical terms when appropriate, "+
+      "but ensure that your answers are accessible to users with varying levels of gardening knowledge. "+
+      "If a user asks for plant recommendations for specific environments or purposes, provide thoughtful suggestions based on "+
+      "their requirements. Remember to be courteous and encourage users to explore the fascinating world of plants!" +
+      " Topic is +" + userMessage;
 
     const result = await chat.sendMessage(userMessage);
     const response = await result.response;
     const text = response.text();
-    return text
+    return text;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
